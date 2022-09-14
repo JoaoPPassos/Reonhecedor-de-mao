a002694540752c7ff18734c5b077261eb4b517b9
JoaoPPassos <jp.passos@squadts.com.br>
2022-09-14 17:26:21 -0300

    feat: pronto pra começar testes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 import argparse
-from PIL import Image
+
+from cv2 import resizeWindow
 
 vid = cv.VideoCapture(0)
 
 right = cv.CascadeClassifier("template.xml")
 
 template = cv.imread("./maos_reconheciveis/mao 1.png",0)
+handCountour = cv.imread("./ContourHand/hand contourn.png",0)
 th,tw,*_ = template.shape
 
 max_value = 255
@@ -15,6 +17,8 @@ max_binary_value = 255
 trackbar_type = 'Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted'
 trackbar_value = 'Value'
 window_name = 'Threshold Demo'
+
+test_images = []
 
 threshold_type = ""
 threshold_value = ""
@@ -32,10 +36,18 @@ def Threshold_Demo(val):
 
     threshold_type = cv.getTrackbarPos(trackbar_type, window_name)
     threshold_value = cv.getTrackbarPos(trackbar_value, window_name)
+
+def setTestImages():
+  for i in range(1 ,6):
+    test_images.append("imagem_{}.jpeg".format(i))
     
+setTestImages()
+
 def detectAndDisplay(frame):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
+    # frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    global threshold_type 
+    global threshold_value
+    frame_gray = cv.equalizeHist(frame)
     
     #0: Binary
     #1: Binary Inverted
@@ -48,13 +60,14 @@ def detectAndDisplay(frame):
     righthand = right.detectMultiScale(frame_gray,1.2,7)
     # cv.rectangle(frame_gray, ())
     # cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+    _, dst = cv.threshold(frame_gray, threshold_value, max_binary_value, threshold_type )
+    cv.imshow("threshold",dst)
     
     if righthand == ():
       # print("RightHand not available")
       x = 0
     else:
-      global threshold_type 
-      global threshold_value
+      
       
       for(x,y,w,h) in righthand:
         # cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
@@ -69,24 +82,60 @@ def detectAndDisplay(frame):
         cv.imshow("hand",dst)
         cv.resizeWindow("hand",200,400)
     
-  
+
+def checkHands():
+  for url in test_images:
+    image = cv.imread("Maos/{}".format(url))
+    
+    frame_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)
+    
+    righthand = right.detectMultiScale(frame_gray,1.2,7)
+
+    if righthand == ():
+      # print("RightHand not available")
+      x = 0
+    else:
+      global threshold_type 
+      global threshold_value
+      for(x,y,w,h) in righthand:
+        # cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+        cropimage = frame_gray[y:y+th,x:x+tw]
+        # frame_HSV = cv.cvtColor(cropimage, cv.COLOR_BGR2HSV)
+        # frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+   
+        _, dst = cv.threshold(cropimage, threshold_value, max_binary_value, threshold_type )
+        
+        # test = cv.Canny(cropimage,150,180)
+        cv.imshow("hand_test",dst)
+        handChecker(dst)
+        # cv.resizeWindow("hand_test",200,400)
+        
 def handChecker(frame):
   result = cv.matchTemplate(frame,template,cv.TM_CCORR_NORMED)
-  cv.imshow("calc", result)
-  if(result[0][0] > 0.5):
-      print(result[0])
+
+  if(result[0][0] > 0.58):
+      print("result ===>",result[0])
 
   
-cv.createTrackbar(trackbar_type, window_name , 3, max_type,Threshold_Demo)
-cv.createTrackbar(trackbar_value, window_name , 1, max_value,Threshold_Demo)
+cv.createTrackbar(trackbar_type, window_name , 1, max_type,Threshold_Demo)
+cv.createTrackbar(trackbar_value, window_name , 27, max_value,Threshold_Demo)
 
+
+#typo 1 valor 27
 
 while (vid.isOpened()):
   ret,frame = vid.read()
 
-  detectAndDisplay(frame)
+  # checkHands()
+  resized_frame = cv.cvtColor(cv.resize(frame,(500,500)), cv.COLOR_BGR2GRAY)
+  # handCountourGrey = cv.cvtColor(handCountour,cv.COLOR_BGR2GRAY)
   
+  andimage = cv.bitwise_and( handCountour,resized_frame)
   
+  detectAndDisplay(resized_frame)
+
+  cv.imshow("hand 2",andimage)
   cv.imshow(window_name, frame)
 
   if cv.waitKey(1) == ord('q'):
